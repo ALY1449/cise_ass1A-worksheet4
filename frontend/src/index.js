8e391de0f87d9f2e824952c1ab88cc53069e5d23
Alyssa Pausanos <alyssapausanos@Alyssas-MacBook-Pro-2.local>
2022-08-16 15:37:41 +1200

    first commit

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-no-undef */
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./App";
+ 
+ReactDOM.render(
+  <App/>, 
+  document.getElementById("root")
+);
