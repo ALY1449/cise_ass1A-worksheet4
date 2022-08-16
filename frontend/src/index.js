91d399866f38d3b12342858f836101578b47b52c
Alyssa Pausanos <alyssapausanos@Alyssas-MacBook-Pro-2.local>
2022-08-17 11:01:12 +1200

    add dummy data files and UI

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
 /* eslint-disable react/jsx-no-undef */
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./App"; //error here, should be App instead of Main
+import "./index.css";
  
 ReactDOM.render(
   <App/>, 
